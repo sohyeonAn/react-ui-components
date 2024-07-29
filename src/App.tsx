0a605db2fd8946ffb043af120160ca95bdaee73c
sohyeonAn <ansh4393@gmail.com>
2024-07-29 18:16:28 +0900

    feat: Text 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import './App.css'
+import Text from '@/components/Text'
 
 function App() {
-  return <></>
+  return (
+    <div>
+      <Text typography="t1" display="block" color="red">
+        t1
+      </Text>
+      <Text typography="t2" color="blue">
+        t2
+      </Text>
+      <Text typography="t3" textAlign="right" display="block">
+        t3
+      </Text>
+      <Text typography="t4" display="block" bold>
+        t4
+      </Text>
+      <Text>t5</Text>
+      <Text typography="t6" color="green">
+        t6
+      </Text>
+      <Text typography="t7" display="inline-block" color="grey">
+        t7
+      </Text>
+    </div>
+  )
 }
 
 export default App
