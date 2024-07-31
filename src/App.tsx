37ac3a62e7fbdb38c2696240db3dd670ebe6966d
sohyeonAn <ansh4393@gmail.com>
2024-07-31 17:59:14 +0900

    feat: Router 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from '@/pages/Home'
+import Test from '@/pages/Test'
 
 function App() {
-  return <div></div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/test" Component={Test} />
+        <Route path="*" Component={Home} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
